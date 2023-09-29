11f534a48fa843d12f3461066824d71be06e57e8
Gilberto Borboa <elswitch212@gmail.com>
2023-09-29 08:40:55 -0700

    agregar un global y una funcion

--- hola_mundo.js
+++ hola_mundo.js
@@ -2,4 +2,10 @@ console.log('Hola mundo desde Node.js, esto se vera en la terminal de comando')
 
 console.log(2*5)
 
-console.log(window)
+//console.log(window)
+
+console.log(global)
+
+setInterval(function(){
+    console.log('hola nodejs')
+},1000)

019459d9c9537895e3ba4e18f0a197c096a00847
Gilberto Borboa <elswitch212@gmail.com>
2023-10-06 09:18:07 -0600

    servidor funcionando con texto html

--- server_http.js
+++ server_http.js
@@ -4,8 +4,8 @@ const port = 3000 //establecemos el puerto de escucha
 
 const server = http.createServer((req,res)=>{ //creamos una instancia HTTP con un reques y un respond
     res.statusCode = 200; //wl servidor respondera un codigo 200
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola mundo\n')
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hola mundo</h1>');
 });
 
 server.listen(port, hostname, ()=>{
